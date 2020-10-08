b498ee5cb678286632116c8671ba29b6070c4101
Milton Boos Junior <junior_boos@live.com>
2020-10-08 16:42:48 +0100

    initial commit

---
+++ src/services/firebase.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mqtt-teste-iot.firebaseio.com"
+});
