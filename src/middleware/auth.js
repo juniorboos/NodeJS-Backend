b89795b16d68b75c231e196f083ffc7f3ec50034
Milton Boos Junior <junior_boos@live.com>
2020-10-25 01:44:16 +0100

    added env file

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
-
+require('dotenv/config')
 module.exports = async (req, res, next) => {
    const authHeader = req.headers.authorization;
-
    if (!authHeader) {
       return res.status(401).send({ error: "No token provided" });
    }
@@ -11,12 +10,13 @@ module.exports = async (req, res, next) => {
    const [scheme, token] = authHeader.split(" ");
 
    try {
-      const decoded = await promisify(jwt.verify)(token, "secret");
+      const decoded = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN_SECRET);
 
       req.userId = decoded.id;
 
       return next();
    } catch (err) {
+      console.log(err)
       return res.status(401).send({ error: "Token invalid" });
    }
 };
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const RegionController = require('./controllers/RegionController');
 const SpotController = require('./controllers/SpotController');
 const cookieParser  = require("cookie-parser")
 const authMiddleware = require('./middleware/auth')
-
+require('dotenv/config')
 const routes = express.Router();
 const admins = require('./services/Admins.json')
 
@@ -24,8 +24,7 @@ routes.post("/authenticate", async (req, res) => {
          return res.status(400).json({ error: "Invalid password" });
       }
 
-      const token = jwt.sign({ id: this.id }, "secret", { expiresIn: 86400 })
-
+      const token = jwt.sign({ id: this.id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 86400 })
       res.cookie('token', token, { httpOnly : true})
 
       res.json({
